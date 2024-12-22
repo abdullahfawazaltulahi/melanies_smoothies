d43c76fae15b4f8cced6330ac607f20b3c0175fa
Abdullah Altulahi <63244923+abdullahfawazaltulahi@users.noreply.github.com>
2024-12-22 14:36:30 +0300

    Update streamlit_app.py
    adding json for front for api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write(
 # st.write('you selected: ',option)
 # new section to dispaly smoothiesfroot nutrition information
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 name_on_order = st.text_input('Name on Smoothies:')
 st.write('the name on your smoothies will be: ',name_on_order)
 cnx = st.connection("snowflake")
